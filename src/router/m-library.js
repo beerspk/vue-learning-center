ac34f4f8f091a9dcefadb45cdeca43285705f795
Pamorn.s <pamorn.si@gmail.com>
2021-12-21 08:23:31 +0700

    adding overview

--- src/router/m-library.js
+++ src/router/m-library.js
@@ -1,6 +1,7 @@
 import Home from "@/views/m-library/Home";
 import Join from "@/views/m-library/Join";
 import Main from "@/views/m-library/Main";
+import CourseOverview from "@/views/m-library/CourseOverview";
 
 const m_library_route = [
   {
@@ -17,6 +18,11 @@ const m_library_route = [
     path: "/main",
     name: "Main",
     component: Main,
+  },
+  {
+    path: "/course/overview/:id",
+    name: "CourseOverview",
+    component: CourseOverview,
   },
 ];
 
--- src/views/m-library/CourseOverview.vue
+++ src/views/m-library/CourseOverview.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="CourseOverview">
-
+    
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {}
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
